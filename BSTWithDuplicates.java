304c18ff6e90db69bddd42151e7a6962be0f5261
nathanml <34776276+nathanml@users.noreply.github.com>
2018-03-25 19:52:20 -0400

    Update BSTWithDuplicates.java

--- BSTWithDuplicates.java
+++ BSTWithDuplicates.java
@@ -8,19 +8,6 @@ package hw7;
 
 import java.util.LinkedList;
 import java.util.Random;
-/**
- * Binary search tree, keyed by integers with String data, which
- * allows for insertion of the same key twice (or more), and will create
- * a new node for it each time.  The new node may be either to
- * the right or to the left of a higher node with the same
- * key; the direction for a duplicate key
- * is chosen at random, else duplicate keys
- * could create very long chains and hamper performance (or even
- * cause stack overflow errors on recursion).
- * Has a special search, which returns a LinkedList
- * of data for all the keys within range.
- * 
- */
 
 public class BSTWithDuplicates {
 	private class TreeNode {
@@ -35,21 +22,13 @@ public class BSTWithDuplicates {
 	private TreeNode root = null;
 	Random random = new Random();
 
-	/**
-	 * Adds (key, data) pair to the tree, even if the same key
-	 * already exists.
-	 * @param key
-	 * @param data
-	 */
+
 	public void insert (int key, String data) {
 		root = insert (key, data, root);
 	}
 
 
-private TreeNode insert (int key, String data, TreeNode subtree) {
-//		// TODO: implement
-//		// Use random.nextBoolean to decide whether to go right or left
-//		// when key is equal to subtree.key		
+private TreeNode insert (int key, String data, TreeNode subtree) {	
 		if(subtree == null)
 		{
 			return new TreeNode(key, data);
@@ -65,13 +44,7 @@ private TreeNode insert (int key, String data, TreeNode subtree) {
 		return subtree;
 	}
 
-	/**
-	 * Assumes low<=high
-	 * 
-	 * @param low
-	 * @param high
-	 * @return a collection of all data elements whose keys are between low and high (inclusive)
-	 */
+
 	public LinkedList<String> rangeSearch (int low, int high) {
 		LinkedList<String> ret = new LinkedList<String>();
 		rangeSearch (root, low, high, ret);
@@ -79,10 +52,6 @@ private TreeNode insert (int key, String data, TreeNode subtree) {
 	}
 
 	private void rangeSearch (TreeNode subtree, int low, int high, LinkedList<String> ret) {
-		// TODO implement
-		// suggestion: check if subtree key is less than low, greater than high,
-		// or between the two; use this check to decide what to add
-		// to ret and whether to recurse right or left or both
 			if(subtree == null)
 				return;
 			if(subtree.key >= low)
@@ -99,10 +68,6 @@ private TreeNode insert (int key, String data, TreeNode subtree) {
 			}
 	}
 
-
-	/**
-	 * @return the largest key in the tree; 0 if the key is empty
-	 */
 	public int maxKey() {
 		return _maxKey(this.root);
 	}
